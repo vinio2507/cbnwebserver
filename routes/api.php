33b7fd5a8134ddfba905184b4c86d8258b571a31
Vinicius Berto <vinio2507@gmail.com>
2019-12-16 17:32:41 -0400

    Correção do cors request

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::middleware('cors')->post('save-data','GlobalController@store');
-Route::middleware('cors')->get('get-data/{name}','GlobalController@show');
+Route::post('save-data','GlobalController@store');
+Route::get('get-data/{name}','GlobalController@show');
