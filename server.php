aaff932d9198043b2c5ff0849f471fefbb0a5761
Vinicius Berto <vinio2507@gmail.com>
2019-12-18 13:35:34 -0400

    Correção do cors request

--- server.php
+++ server.php
@@ -11,7 +11,7 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-
+//Teste
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
